398e6fd789592f871b6c480c0680a30bbb74da9d
1seanr <1seanr@gmail.com>
2017-04-28 17:00:06 +1200

    V4) Smell 4 - Large Method
    The original large method has been split up into 4 smaller more defined methods
    Original Method:
        __data_validation_handler
    New Methods:
        __data_validation_handler
        __search_through_data_and_build_results
        __sort_and_check_row_data
        __birth_date_check
    
    These methods are allot more focused and complete 1 task each rather then 1 method completing multiple different tasks
    The code now follows the single responsibility rule and also is allot more readable.
    
    Full Documentation in provided word docs submitted on Moodle

--- validating_input.py
+++ validating_input.py
@@ -6,6 +6,12 @@ class ValidateData:
     def __init__(self, data_list):
         self.__result_list = []
         self.__age = 0
+        self.__regexp_list = ["^[A-Z][0-9]{3}$",
+                              "(^(M|m)$|^(F|f)$|^(M|m)ale$|^(F|f)emale$)",
+                              "^[0-9]{2}$", "^[0-9]{3}$",
+                              "(^(N|n)ormal$|^(O|o)verweight$|^(O|o)bese"
+                              "$|^(U|u)nderweight$)",
+                              "^[0-9]{2,3}$", ""]
         self.__data_validation_handler(data_list)
 
     def __date_check(self, day, month, year):
@@ -32,55 +38,60 @@ class ValidateData:
 
     def __data_validation_handler(self, data_list):
         self.__result_list = []
-        regexp_list = ["^[A-Z][0-9]{3}$",
-                       "(^(M|m)$|^(F|f)$|^(M|m)ale$|^(F|f)emale$)",
-                       "^[0-9]{2}$", "^[0-9]{3}$",
-                       "(^(N|n)ormal$|^(O|o)verweight$|^(O|o)bese$|^("
-                       "U|u)nderweight$)",
-                       "^[0-9]{2,3}$", ""]
         outer_list_pos = 0
         for row in data_list:
-            temp_result_list = []
-            inner_list_pos = 0
-            try:
-                # Going through all of the items in each row
-                for col in row:
-                    if col != "":
-                        if inner_list_pos == 2:
-                            self.__age = int(float(col))
-                        # This is to check the birth date using date
-                        # check method
-                        if inner_list_pos == 6:
-                            try:
-                                col = col.replace("/", "-").replace(" ", "-")
-                                try:
-                                    day, month, year = col.split("-")
-                                    temp_result_list.append(
-                                        self.__date_check(int(float(day)),
-                                                          int(float(month)),
-                                                          int(float(year))))
-                                except ValueError:
-                                    pass
-                                if len(temp_result_list) != len(regexp_list):
-                                    temp_result_list.append(False)
-                            except AttributeError:
-                                temp_result_list.append(False)
-                        # If its not the birth date its checking it
-                        # uses the regex checking method
-                        elif isinstance(col, float):
-                            temp_result_list.append(self.__reg_exp_checker(
-                                regexp_list[inner_list_pos], str(int(col))))
-                        else:
-                            temp_result_list.append(self.__reg_exp_checker(
-                                regexp_list[inner_list_pos], col))
-                    else:
-                        temp_result_list.append(False)
-                    inner_list_pos += 1
-            except IndexError:
-                temp_result_list = (False, False, False, False, False,
-                                    False, False)
+            temp_result_list = self.__search_through_data_and_build_results(
+                row)
             self.__result_list.append(temp_result_list)
             outer_list_pos += 1
+
+    def __search_through_data_and_build_results(self, row):
+        temp_result_list = []
+        inner_list_pos = 0
+        try:
+            # Going through all of the items in each row
+            for col in row:
+                if col != "":
+                    self.__sort_and_check_row_data(col, inner_list_pos,
+                                                   temp_result_list)
+                else:
+                    temp_result_list.append(False)
+                inner_list_pos += 1
+        except IndexError:
+            temp_result_list = (False, False, False, False, False,
+                                False, False)
+        return temp_result_list
+
+    def __sort_and_check_row_data(self, col, inner_list_pos, temp_result_list):
+        if inner_list_pos == 2:
+            self.__age = int(float(col))
+        # This is to check the birth date using date check method
+        if inner_list_pos == 6:
+            self.__birth_date_check(col, temp_result_list)
+        # If its not the birth date its checking it uses the regex
+        # checking method
+        elif isinstance(col, float):
+            temp_result_list.append(self.__reg_exp_checker(
+                self.__regexp_list[inner_list_pos], str(int(col))))
+        else:
+            temp_result_list.append(self.__reg_exp_checker(
+                self.__regexp_list[inner_list_pos], col))
+
+    def __birth_date_check(self, col, temp_result_list):
+        try:
+            col = col.replace("/", "-").replace(" ", "-")
+            try:
+                day, month, year = col.split("-")
+                temp_result_list.append(
+                    self.__date_check(int(float(day)),
+                                      int(float(month)),
+                                      int(float(year))))
+            except ValueError:
+                pass
+            if len(temp_result_list) != len(self.__regexp_list):
+                temp_result_list.append(False)
+        except AttributeError:
+            temp_result_list.append(False)
 
     def get_results(self):
         return self.__result_list
